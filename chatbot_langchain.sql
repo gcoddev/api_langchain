d48afe93f5f699dfb3c85c505b0bc1d23d54df22
gcoddev <geco.yak77@gmail.com>
2024-06-18 01:20:19 -0400

    sql

--- chatbot_langchain.sql
+++ chatbot_langchain.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 15-06-2024 a las 15:39:07
+-- Tiempo de generación: 18-06-2024 a las 04:56:00
 -- Versión del servidor: 11.4.2-MariaDB
 -- Versión de PHP: 8.3.8
 
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `chatbot_langchain`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id_blog` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `contenido` text NOT NULL,
+  `estado` enum('0','1','2') NOT NULL DEFAULT '1',
+  `creado_el` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -86,14 +103,37 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `ci`, `expedido`, `paterno`, `materno`, `nombres`, `nacimiento`, `celular`, `email`, `username`, `password`, `estado`, `id_rol`) VALUES
-(1, '1', 'LP', 'Admin', 'Admin', 'Admin', '2001-12-01', 1, 'admin@admin.com', 'admin', '$2a$10$Nalv9RXtkTV4BTka0VI6VOvkp02CninruO4boEwFXbXtY1K/YRJUG', '1', 1),
+(1, '1', 'LP', 'Admin', 'Admin', 'Admin', '2001-12-01', 1, 'admin@admin.com', 'admin', '$2a$10$.CjPHy9ZuUvBgwFUmsAzoO0Q2guze2lKWgwZy/1tUHVMDTZ5c1ega', '1', 1),
 (2, '2', 'LP', 'Prueba', 'Prueba', 'Prueba1', '2001-12-01', 2, 'prueba@admin.com', 'prueba', '$2a$10$Ny8sXbIHXfEI4xJ9lMcjOej8X3CxzdiXYDurVYl6NfRasEcwUZrca', '1', 2),
 (3, '3', 'LP', 'Prueba', 'Prueba', 'Prueba2', '2001-12-01', 3, 'prueba@admin.com', 'prueba2', '$2a$10$6fPMiIAq1OM3gImAn9UGAuArsAnXoRg4GCwpaK8/KBiHJMWlT.Ksq', '1', 3),
-(5, '3', 'LP', 'Prueba', 'Prueba', 'Prueba2', '2001-12-01', 3, 'prueba@admin.com', 'prueba3', '$2a$10$DQzZ.VUQPXreJkF6F6sfU.z4DOFiPo2aHRHJC5VMbYJrH71eCW79C', '2', 3);
+(5, '3', 'LP', 'Prueba', 'Prueba', 'Prueba2', '2001-12-01', 3, 'prueba@admin.com', 'prueba3', '$2a$10$DQzZ.VUQPXreJkF6F6sfU.z4DOFiPo2aHRHJC5VMbYJrH71eCW79C', '0', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videos`
+--
+
+CREATE TABLE `videos` (
+  `id_video` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `estado` enum('0','1','2') NOT NULL DEFAULT '1',
+  `creado_el` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id_blog`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indices de la tabla `documentos`
@@ -117,8 +157,21 @@ ALTER TABLE `usuarios`
   ADD KEY `id_rol` (`id_rol`);
 
 --
+-- Indices de la tabla `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id_video`),
+  ADD KEY `id_user` (`id_user`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id_blog` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `documentos`
@@ -139,6 +192,12 @@ ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de la tabla `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -153,6 +212,12 @@ ALTER TABLE `documentos`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `fk_rol_usuario` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+
+--
+-- Filtros para la tabla `videos`
+--
+ALTER TABLE `videos`
+  ADD CONSTRAINT `fk_videos_user` FOREIGN KEY (`id_user`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- load_pdf.py
+++ load_pdf.py
@@ -7,44 +7,44 @@ from langchain.vectorstores import FAISS
 
 
 def create_embeddings(pdf_directory, embedding_file):
-    try:
-        if os.path.exists(embedding_file):
-            os.remove(
-                embedding_file
-            )  # Elimina el archivo de embeddings existente si existe
-
-        pdf_files = [f for f in os.listdir(pdf_directory) if f.endswith(".pdf")]
-        if not pdf_files:
-            print("No PDF files found in the directory.")
-            return None
-
-        text = ""
-        for pdf_file in pdf_files:
-            pdf_path = os.path.join(pdf_directory, pdf_file)
-            print(f"Loading PDF: {pdf_path}")  # Mensaje de carga del PDF
-            pdf_reader = PdfReader(pdf_path)
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=800, chunk_overlap=100, length_function=len
+    # try:
+    if os.path.exists(embedding_file):
+        os.remove(
+            embedding_file
         )
-        chunks = text_splitter.split_text(text)
 
-        embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-        )
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
+    pdf_files = [f for f in os.listdir(pdf_directory) if f.endswith(".pdf")]
+    if not pdf_files:
+        print("No PDF files found in the directory.")
+        return None
 
-        with open(embedding_file, "wb") as f:
-            pickle.dump(knowledge_base, f)
-        print("Embeddings created and saved to file.")
+    text = ""
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(pdf_directory, pdf_file)
+        print(f"Loading PDF: {pdf_path}")
+        pdf_reader = PdfReader(pdf_path)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
 
-        return knowledge_base
-    except ImportError as e:
-        print(f"Error importing necessary libraries: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800, chunk_overlap=100, length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    )
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    with open(embedding_file, "wb") as f:
+        pickle.dump(knowledge_base, f)
+    print("Embeddings created and saved to file.")
+
+    return knowledge_base
+    # except ImportError as e:
+    #     print(f"Error importing necessary libraries: {e}")
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
 
 
 pdf_directory = "./public/documentos/"
