e9db240c774f96b5c5d3220b93b51fdcf50cfc86
gcoddev <geco.yak77@gmail.com>
2024-06-19 10:41:40 -0400

    deploy

--- src/routes/router_chatbot.js
+++ src/routes/router_chatbot.js
@@ -4,6 +4,21 @@ const router = express.Router()
 import token from '../libs/keys.js'
 import ControladorChatbot from '../controllers/controller_chatbot.js'
 
+router.route('/')
+    .get(async (req, res) => {
+        const html = await `<!DOCTYPE html><html><head><title>Chatbot langchain</title><body><h1>Welcome chatbot</h1></body></head></html>`
+        res.status(200).send(html)
+    })
+
+router.route('/api')
+    .get((req, res) => {
+        res.status(200).json({
+            status: 'ok',
+            message: 'Chatbot online'
+        });
+
+    })
+
 router.route('/api/ask')
     .post(token.decodeToken, ControladorChatbot.preguntar)
 
